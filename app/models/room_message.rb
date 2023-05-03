1fea65f7b26dae2b7c46bc6207c97c4ccfcab780
Maria Kravtsova <maria.kravtsova@warbyparker.com>
2023-05-03 19:10:42 -0400

    Install indie stack template remix

--- app/models/room_message.rb
+++
@@ -1,4 +0,0 @@
-class RoomMessage < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :room_messages
-  resources :rooms
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root controller: :rooms, action: :index
-end
